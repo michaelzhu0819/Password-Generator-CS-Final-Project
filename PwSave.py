f73b148e563ee4a72bfee71f451670b27113e2b0
michaelzhu0819 <89995944+michaelzhu0819@users.noreply.github.com>
2022-01-22 13:25:57 -0600

    pep8 and comments
    ran throught pep8 along with the addition of comments

--- PwSave.py
+++ PwSave.py
@@ -1,8 +1,13 @@
+# importing required modules
 import time
+
+
+"""This is the function that saves the password"""
 
 
 def file_save(password):
 
+    # setting up required variables
     count = 1
     checkp_1 = 1
     save_p = []
@@ -10,28 +15,33 @@ def file_save(password):
 
     time.sleep(1)
 
+    # prints all stored passwords
     for i in password:
         print(f"{count} - {i}")
         count += 1
 
     time.sleep(1)
 
+    # checkpoint system to accept wrong inputs
+    # asks which passwords the users wants to save
     while checkp_1 != 0:
         num_ask = input("which of the following do you want to save? input " +
                         "the number in front of the password. \nSeparate by " +
                         "space and enter all before pressing enter\n")
         num_ask = num_ask.split(" ")
 
+        # handles error
         for i in num_ask:
             try:
                 if int(i) > len(password):
-                    print("one or more of the index you chose was out of range, " +
-                          "please try again\n")
+                    print("one or more of the index you chose was out of " +
+                          "range, please try again\n")
                     break
             except ValueError:
                 print("you did not input a number!\n")
                 break
-        
+
+        # handles error
         try:
             if int(i) <= len(password):
                 checkp_1 -= 1
@@ -42,6 +52,7 @@ def file_save(password):
 
     time.sleep(1)
 
+    # asks what purpose each pw serves to better create the saved file
     for i in num_ask:
         if int(i) <= len(password):
             save_p.append(password[int(i) - 1])
@@ -49,8 +60,9 @@ def file_save(password):
                               f"{password[int(i) - 1]} for?\n\t - ")
             locat.append(locat_ask)
 
-    f = open("generated password.txt", "w+")
+    f = open("generated password.txt", "w+")  # creates a .txt file
 
+    # writes the pws and their purposes in an organized manner in file
     for i in range(len(num_ask)):
         f.write("\t- the password for " + locat[i] + " is " +
                 save_p[i])
